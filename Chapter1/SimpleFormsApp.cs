6ae09785e8a8fb8e8471884ae97030db24c014f8
Cartea Ann A. Mosqueda <144838000+CarteaMosqueda@users.noreply.github.com>
2025-03-03 10:33:50 +0800

    Add files via upload

---
+++ Chapter1/SimpleFormsApp.cs
@@ -0,0 +1,38 @@
+namespace CarteaAnnMosqueda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string fNameValue = firstname.Text;
+            string lNameValue = lastname.Text;
+            MessageBox.Show($"Hello World: {fNameValue} {lNameValue}", "Greetings", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
+
