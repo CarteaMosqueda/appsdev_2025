854ec82b72835cb5ae4bba3524d7fc2c714c1823
Cartea Ann A. Mosqueda <144838000+CarteaMosqueda@users.noreply.github.com>
2025-03-10 15:33:16 +0800

    Add files via upload

---
+++ Chapter1/SimpleFormsAppWithListBox.cs
@@ -0,0 +1,41 @@
+namespace SimpleFormsAppWithListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !listBox1.Items.Contains(textBox1.Text))
+            {
+
+                listBox1.Items.Add(textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to remove.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
